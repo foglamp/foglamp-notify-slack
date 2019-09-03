7a8fde8cbfc98fc1ddb547575f6e03a00e41911b
pintomax <massimiliano.pinto@gmail.com>
2019-09-03 13:54:08 +0200

    Check for exception in https->sendRequest() (#19)
    Check for exception in https->sendRequest()
    Added generic catch all

--- slack.cpp
+++ slack.cpp
@@ -44,7 +44,7 @@ Slack::~Slack()
 void Slack::notify(const string& notificationName, const string& triggerReason, const string& message)
 {
 ostringstream   payload;
-SimpleHttps	*https;
+SimpleHttps	*https = NULL;
 
 
 	payload << "{ \"text\" : \"";
@@ -63,35 +63,74 @@ SimpleHttps	*https;
 	pair<string, string> header = make_pair("Content-type", "application/json");
 	headers.push_back(header);
 
+	if (m_url.empty())
+	{
+		Logger::getLogger()->error("Slack webhook is not set");
+		return;
+	}
+
 	/**
 	 * Extract host and port from URL
 	 */
-	size_t findProtocol = m_url.find_first_of(":");
-	string protocol = m_url.substr(0,findProtocol);
 
-	string tmpUrl = m_url.substr(findProtocol + 3);
-	size_t findPort = tmpUrl.find_first_of(":");
-	size_t findPath = tmpUrl.find_first_of("/");
-	string port, hostName;
-	if (findPort == string::npos)
+	try
 	{
-		hostName = tmpUrl.substr(0, findPath);
-		https  = new SimpleHttps(hostName);
+		size_t findProtocol = m_url.find_first_of(":");
+		string protocol = m_url.substr(0,findProtocol);
+		
+		string tmpUrl = m_url.substr(findProtocol + 3);
+		size_t findPort = tmpUrl.find_first_of(":");
+		size_t findPath = tmpUrl.find_first_of("/");
+		string port, hostName;
+		if (findPort == string::npos)
+		{
+			hostName = tmpUrl.substr(0, findPath);
+			https  = new SimpleHttps(hostName);
+		}
+		else
+		{
+			hostName = tmpUrl.substr(0, findPort);
+			port = tmpUrl.substr(findPort + 1 , findPath - findPort -1);
+			string hostAndPort(hostName + ":" + port);
+			https  = new SimpleHttps(hostAndPort);
+		}
+
+		int errorCode;
+		if ((errorCode = https->sendRequest("POST",
+						    m_url,
+						    headers,
+						    payload.str())) != 200 &&
+		    errorCode == 202)
+		{
+			Logger::getLogger()->error("Failed to send notification "
+						   "to slack webhook  %s, errorCode %d",
+						   m_url.c_str(),
+						   errorCode);
+		}
 	}
-	else
+	catch (exception &e)
 	{
-		hostName = tmpUrl.substr(0, findPort);
-		port = tmpUrl.substr(findPort + 1 , findPath - findPort -1);
-		string hostAndPort(hostName + ":" + port);
-		https  = new SimpleHttps(hostAndPort);
+		Logger::getLogger()->error("Exception while sending notification "
+					   "to slack webhook  %s: %s",
+					    m_url.c_str(),
+					    e.what());
+
+	}
+	catch (...)
+	{
+		std::exception_ptr p = std::current_exception();
+		string name = (p ? p.__cxa_exception_type()->name() : "null");
+
+		Logger::getLogger()->error("Generic exception found while sending notification "
+					   "to slack webhook  %s: %s",
+					    m_url.c_str(),
+					    name.c_str());
 	}
 
-	int errorCode;
-	if ((errorCode = https->sendRequest("POST", m_url, headers, payload.str())) != 200 && errorCode == 202)
+	if (https)
 	{
-		Logger::getLogger()->error("Failed to send notification to slack webhook  %s, errorCode %d", m_url.c_str(), errorCode);
+		delete https;
 	}
-	delete https;
 }
 
 /**
